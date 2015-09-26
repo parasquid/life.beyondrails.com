2a405f6355aac8d9d7515dac9c26172fbc4df163
parasquid <tristan.gomez@gmail.com>
2015-09-27 03:37:21 +0800

    kill hugo server, no need to specify theme since it should have been specified in the configuration

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-hugo --theme=hyde-x
+killall hugo # make sure no hugo server is running since that can mess things up
+hugo
 s3cmd sync public/* s3://life.beyondrails.com -v --no-mime-magic
